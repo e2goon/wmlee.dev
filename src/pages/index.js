ef2bdbe19093a18c38be6ef2fe0637d9a9d8754e
wonmin, lee <e2goon@gmail.com>
2020-06-25 01:16:05 +0900

    feat: Blog Post 기능

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,40 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="프론트엔드 개발자 이원민입니다." />
-    <h1>아직 뚝딱뚝딱 중</h1>
+    {data.allMarkdownRemark.edges && (
+      <ul>
+        {data.allMarkdownRemark.edges.map(({ node }) => {
+          return (
+            <li>
+              <Link to={node.frontmatter.path}>{node.frontmatter.title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    )}
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          html
+          frontmatter {
+            date
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
