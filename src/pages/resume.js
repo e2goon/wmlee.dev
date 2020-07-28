b123b294bf7e6ee00a8cce9df05c80448d428a29
wonmin, lee <e2goon@gmail.com>
2020-07-29 01:34:50 +0900

    add: 이력서 페이지 초안 추가

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -19,11 +19,8 @@ function ResumePage() {
       }
     }
   `)
-
   const [pause, setPause] = React.useState(false)
-
   const timer = useRef()
-
   const [sliderRef, slider] = useKeenSlider({
     loop: true,
     duration: 1000,
@@ -34,7 +31,6 @@ function ResumePage() {
       setPause(false)
     },
   })
-
   useEffect(() => {
     timer.current = setInterval(() => {
       if (!pause && slider) {
@@ -72,6 +68,9 @@ function ResumePage() {
           <strong>제 팀에서 컴포넌트 단위로 UI를 설계하고 개발하는 일</strong>을
           하고 있으며 기본적으로 웹접근성, 웹표준을 준수하려 애씁니다.
         </p>
+      </header>
+
+      <Section>
         <h2>저는...</h2>
         <SliderContainer>
           <Slider ref={sliderRef} className="keen-slider">
@@ -101,29 +100,334 @@ function ResumePage() {
             </SliderItem>
           </Slider>
         </SliderContainer>
-      </header>
+      </Section>
 
-      <section>
+      <Section>
         <h2>경력사항</h2>
-        <ul>
-          <li>스마일게이트 스토브</li>
-          <li>엔씨소프트</li>
-          <li>인코어드테크놀로지</li>
-          <li>스마일게이트 인터넷</li>
-          <li>프리챌</li>
-          <li>미디어포스</li>
-          <li>바른손엔터테인먼트</li>
-        </ul>
-        <details>
-          <summary>경력사항 자세히 보기</summary>
-        </details>
-      </section>
 
-      <section>
+        <Details>
+          <Summary>
+            스마일게이트 스토브{` `}
+            <Badge>2015년 &middot; 5년 5개월 재직중</Badge>
+          </Summary>
+          <DetailsContent>
+            <BadgeList role="list">
+              <Badge role="listitem">HTML</Badge>
+              <Badge role="listitem">CSS</Badge>
+              <Badge role="listitem">Vue</Badge>
+              <Badge role="listitem">TypeScript</Badge>
+              <Badge role="listitem">Atomic Design</Badge>
+              <Badge role="listitem">Storybook</Badge>
+              <Badge role="listitem">Webpack</Badge>
+              <Badge role="listitem">Gulp</Badge>
+              <Badge role="listitem">Nunjucks</Badge>
+              <Badge role="listitem">Bootstrap</Badge>
+              <Badge role="listitem">SASS</Badge>
+              <Badge role="listitem">LESS</Badge>
+              <Badge role="listitem">
+                크로스브라우징 &middot; 웹표준 &middot; 웹접근성
+              </Badge>
+            </BadgeList>
+            <ul>
+              <li>
+                <strong>가칭 길드 커뮤니티</strong>
+                <ul>
+                  <li>
+                    CDD 방식 Vue + TypeScript 환경에서 Storybook 이용한 컴포넌트
+                    개발
+                  </li>
+                  <li>Vue Emoji Picker 컴포넌트 개발</li>
+                  <li>
+                    서비스 요구사항에 적합한 Scrollbar, Range Slider, Markdown
+                    Preview 라이브러리 조사 및 R&D
+                  </li>
+                  <li>
+                    Storybook 선행 도입이후 진행 상황과 장단점들을 유관부서에
+                    전파 및 공유
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <strong>툰스푼 모바일</strong>
+                <ul>
+                  <li>
+                    페이지 로딩 속도 개선을 위한 Image Lazy Loading 컴포넌트
+                    개발 및 적용
+                  </li>
+                  <li>
+                    분업화된 업무 환경에서 Vue 컴포넌트를 직접 운영하여
+                    웹접근성, 코드품질 개선
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <strong>동적템플릿 TF</strong> 한가지의 HTML 구조로 다양한
+                레이아웃 타입을 구현하는 방안들을 연구
+              </li>
+              <li>
+                <strong>공통 GNB</strong> 사내에서 사용하고 외부에 제공해야하는
+                B2B 서비스로 Webpack 으로 구성한 프로젝트에 GNB 마크업 운영 및
+                버전관리, 업데이트 기록 후 전파
+              </li>
+              <li>
+                <strong>사내 디자인 시스템</strong> 가이드라인을 nuxt.js를
+                이용하여 구축
+              </li>
+              <li>
+                <strong>사내 마크업 가이드</strong> vuepress 를 이용하여 사내
+                마크업 가이드 구축
+              </li>
+              <li>
+                <strong>마크업</strong>
+                <ul>
+                  <li>
+                    다양한 디바이스의 웹뷰와 연동하는 과정에서 많은 문제들을
+                    경험하고 해결해온 경험과 노하우 축적
+                  </li>
+                  <li>주요 게임의 인터렉션, PC, 모바일, 반응형웹 마크업</li>
+                  <li>흩어진 약관을 한 곳에서 관리하여 업무효율화 개선</li>
+                  <li>젠데스크 솔루션 테마를 커스텀하여 2년이상 운영 경험</li>
+                  <li>메가포트, 스토브 전반 서비스 구축 및 운영</li>
+                </ul>
+              </li>
+            </ul>
+          </DetailsContent>
+        </Details>
+
+        <Details>
+          <Summary>
+            인코어드테크놀로지{` `}
+            <Badge>2013년 &middot; 6개월</Badge>
+          </Summary>
+          <DetailsContent>
+            <BadgeList role="list">
+              <Badge role="listitem">HTML</Badge>
+              <Badge role="listitem">CSS</Badge>
+              <Badge role="listitem">Angular</Badge>
+              <Badge role="listitem">JavaScript</Badge>
+              <Badge role="listitem">jQuery</Badge>
+              <Badge role="listitem">
+                크로스브라우징 &middot; 웹표준 &middot; 웹접근성
+              </Badge>
+            </BadgeList>
+          </DetailsContent>
+        </Details>
+
+        <Details>
+          <Summary>
+            스마일게이트 인터넷{` `}
+            <Badge>2011년 &middot; 2년 2개월</Badge>
+          </Summary>
+          <DetailsContent>
+            <BadgeList role="list">
+              <Badge role="listitem">HTML</Badge>
+              <Badge role="listitem">CSS</Badge>
+              <Badge role="listitem">JavaScript</Badge>
+              <Badge role="listitem">jQuery</Badge>
+              <Badge role="listitem">SVN</Badge>
+              <Badge role="listitem">
+                크로스브라우징 &middot; 웹표준 &middot; 웹접근성
+              </Badge>
+            </BadgeList>
+          </DetailsContent>
+        </Details>
+
+        <Details>
+          <Summary>
+            프리챌{` `}
+            <Badge>2010년 &middot; 8개월</Badge>
+          </Summary>
+          <DetailsContent>
+            <BadgeList role="list">
+              <Badge role="listitem">HTML</Badge>
+              <Badge role="listitem">CSS</Badge>
+              <Badge role="listitem">JavaScript</Badge>
+              <Badge role="listitem">jQuery</Badge>
+              <Badge role="listitem">
+                크로스브라우징 &middot; 웹표준 &middot; 웹접근성
+              </Badge>
+            </BadgeList>
+            <ul>
+              <li>프리챌의 모든 주요서비스 리뉴얼 진행</li>
+            </ul>
+          </DetailsContent>
+        </Details>
+
+        <Details>
+          <Summary>기타경력{` `}</Summary>
+          <DetailsContent>
+            <ul>
+              <li>
+                엔씨소프트<Badge>계약직</Badge>
+              </li>
+              <li>
+                웹젠<Badge>계약직</Badge>
+              </li>
+              <li>
+                아큐파이 <Badge>스타트업</Badge>
+              </li>
+              <li>
+                미디어포스 {` `}
+                <Badge>웹에이전시 &middot; 파견직</Badge>
+              </li>
+              <li>
+                그래피티{` `}
+                <Badge>웹에이전시</Badge>
+              </li>
+              <li>
+                바른손엔터테인먼트 <Badge>웹디자이너</Badge>
+              </li>
+            </ul>
+            <p>
+              대학 졸업 후 2007년 바른손엔터테인먼트 신입웹디자이너로 입사 후
+              하드코딩에 적성에 맞아 웹퍼블리셔로 전향하기로 마음먹었으며 이후
+              웹표준 스터디, 세미나 스탭 활동, 대회 참여, 업계 선배님들에게 많은
+              가르침과 조언을 받아 웹에이전시 그래피티, 미디어포스에서
+              웹퍼블리셔로 일을 시작하게 되었습니다.
+            </p>
+            <p>
+              그 과정에서 더 새롭고, 더 성장할 수 있는 일을 찾기 위해 모험을
+              하다 자연스럽게 어려운 경제상황과 맞물리면서 계약직 &middot;
+              파견직을 병행하였습니다.
+            </p>
+          </DetailsContent>
+        </Details>
+      </Section>
+
+      <Section>
         <h2>기술경험</h2>
-      </section>
+        <Details>
+          <Summary>
+            HTML/CSS <Badge>상</Badge>
+          </Summary>
+          <DetailsContent>
+            <ul>
+              <li>
+                디자인 중심이 아닌, 사용자 중심에서 웹접근성을 고민합니다.
+                사용자가 특수한 상황에서 HTML 콘텐츠를 접했을때 이해하기 쉽도록
+                부가 설명을 수록하는 등 소소한 노력을 해왔습니다.
+              </li>
+              <li>
+                기본적으로 웹표준/웹접근성을 준수하려고 노력합니다. 컴포넌트
+                주도 개발 시 구조, 표현, 동작 분리 원칙을 적용하는 것을
+                고려하면서 의미있는 태그들을 선택하여 사용하려고 노력합니다.
+              </li>
+              <li>
+                과거의 IE6 크로스브라우징에 대한 경험을 많이 보유하고 있으며,
+                브라우저 버전별로 지원하기 위해 CSS Hack을 잘 사용할 수 있는
+                노하우를 갖고 있습니다.
+              </li>
+              <li>
+                과거의 SMACSS 방법론 현재의 BEM 및 OOCSS 방법론의 차이를
+                이해하고 있습니다. 현재는 프로젝트 상황에 맞게 방법론을 섞어서
+                사용합니다. 토이프로젝트에서는 OOCSS 방법론을 선호합니다.
+              </li>
+              <li>
+                축적된 노하우와 좋은 솔루션으로 더 빠르게 마크업할 수 있습니다.
+                웹표준/웹접근성 준수는 덤입니다.
+              </li>
+            </ul>
+          </DetailsContent>
+        </Details>
+        <Details>
+          <Summary>
+            Git <Badge>중상</Badge>
+          </Summary>
+          <DetailsContent>
+            <ul>
+              <li>터미널을 이용하여 능숙하게 Git 버전 관리를 합니다.</li>
+              <li>
+                원활한 협업을 위해 커밋을 어떻게 해야하는지 이해하고 있으며
+                팀내에서 1년 넘게 코드리뷰를 진행하고 있습니다.
+              </li>
+              <li>
+                커밋로그를 최대한 더럽히지 않으려고 하며 컨벤션을 잘 따릅니다.
+              </li>
+              <li>
+                한가지의 레거시가 여러가지로 쪼개지면서 누적된 기술 부채들을
+                다시 병합하는 대형 작업을 진행해본 경험이 있습니다.
+              </li>
+            </ul>
+          </DetailsContent>
+        </Details>
+        <Details>
+          <Summary>
+            Vue <Badge>중</Badge>
+          </Summary>
+          <DetailsContent>
+            <ul>
+              <li>
+                Vue Emoji Picker 컴포넌트를 개발하면서 대용량 데이터를 처리하는
+                Virtual Scroll 기법, Lazy Loading 기법을 적용하는 방법을
+                공부하였습니다.
+              </li>
+              <li>
+                툰스푼 프로젝트에 Lazy Image Loading 컴포넌트를 개발하여 상용에
+                적용하여 페이지 로딩 속도에 기여하였습니다.
+              </li>
+              <li>
+                Vue - TypeScript - Storybook의 궁합이 잘 맞지 않는 다는 점을
+                React 와 함께 공부하면서 느낀 점이 많습니다. 이상한 난관들과
+                많은 문제점들을 해결해왔으며 지금은 잘 사용하고 있습니다.
+              </li>
+              <li>
+                토이프로젝트에서는 상태관리 Vuex 스토어에 사용방법을 이해하고
+                있으며 개발자 도구를 활용하여 문제를 해결하는 방법까지 숙지하고
+                있습니다.
+              </li>
+            </ul>
+          </DetailsContent>
+        </Details>
+        <Details>
+          <Summary>
+            React <Badge>하</Badge>
+          </Summary>
+          <DetailsContent>
+            <p>
+              회사에서 React를 경험해볼 수 있는 기회가 없어 토이프로젝트를 하고
+              있습니다. 첫번째는 회사 업무 요구사항의 일부를 React로 구현해보는
+              작업, 두번째는 Gatsby로 개인블로그를 만드는 것. 세번째는 CDD
+              방식으로 회원가입 페이지를 개발하는 일입니다.
+            </p>
+            <ul>
+              <li>
+                컴포넌트 주도 개발을 위해 <code>styled-components</code> 를
+                사용해본 경험이 있습니다. next.js 가 제공하는{" "}
+                <code>styled-jsx</code> 도 사용하는데 거부감이 없습니다.
+              </li>
+              <li>
+                프로젝트의 요구사항에 맞게 서버사이드렌더링, 정적 사이트
+                생성기들의 장단점을 비교하고 선택할 수 있으며 Next.js, Gatsby 의
+                차이점을 이해하고 있습니다.
+              </li>
+              <li>
+                스토리북과 연동하여 회원가입 페이지를 구성하고 이후에{" "}
+                <code>styled-components</code> 라이브러리를 이용하여 CSS-in-JS
+                방식으로 코드를 리팩토링하는 공부를 하였습니다.
+              </li>
+              <li>
+                리액트의 생명주기, 함수 방식, 클래스 방식의 차이점이나 Hook을
+                사용해본 경험이 많지 않으나 토이프로젝트를 진행하면서 최소한의
+                사용법을 조금씩 숙지하여 경험하고 있습니다.
+              </li>
+            </ul>
+          </DetailsContent>
+        </Details>
+        <Details>
+          <Summary>기타</Summary>
+          <DetailsContent>
+            <ul>
+              <li>
+                타입 정의를 하는데 많은 고민을 하는데 시간을 잡아먹었습니다. 이
+                불필요한 시간을 줄이기 위해 정해둔 목표가 끝나면 가장 먼저
+                타입스크립트의 기본기를 공부할 계획입니다.
+              </li>
+            </ul>
+          </DetailsContent>
+        </Details>
+      </Section>
 
-      <section>
+      <Section>
         <h2>경력활동</h2>
         <ul>
           {/* http://158.199.130.218/gallery/ */}
@@ -143,11 +447,7 @@ function ResumePage() {
           <li>정보처리기능사</li>
           <li>컴퓨터그래픽스 운용기능사</li>
         </ul>
-      </section>
-
-      <section>
-        <h2>연락하기</h2>
-      </section>
+      </Section>
     </Layout>
   )
 }
@@ -174,6 +474,10 @@ const JumboImage = styled(Image)`
   border-radius: 1rem;
   border: 4px solid #000;
   box-shadow: 4px 4px 0 0 #232dff;
+`
+
+const Section = styled.section`
+  margin: 4rem 0;
 `
 
 const SliderContainer = styled.div`
@@ -206,4 +510,91 @@ const Card = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
+`
+
+const BadgeList = styled.div`
+  margin: 0 -4px;
+`
+
+const Badge = styled.span`
+  margin: 0 4px;
+  display: inline-block;
+  padding: 0.25em 0.4em;
+  font-size: 75%;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: baseline;
+  border-radius: 0.25rem;
+  background-color: #eee;
+  color: #333;
+`
+
+const Details = styled.details`
+  margin: 2rem 0;
+  display: block;
+  position: relative;
+  ::before {
+    content: "";
+    height: 100%;
+    left: 0.22rem;
+    position: absolute;
+    top: 2rem;
+    height: calc(100% - 2rem);
+    border-left: 1px solid #ddd;
+  }
+`
+
+const DetailsContent = styled.div`
+  padding: 0.6rem 1.2rem;
+  font-size: 1rem;
+  > :first-child {
+    margin-top: 0;
+  }
+  > :last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    padding-left: 1.6rem;
+    li {
+      margin: 1rem 0;
+      :last-of-type {
+        margin-bottom: 0;
+      }
+      li {
+        margin: 0.4rem 0;
+      }
+    }
+  }
+`
+
+const Summary = styled.summary`
+  position: relative;
+  display: block;
+  padding-left: 1.2rem;
+  outline: 0;
+  line-height: 1;
+  cursor: pointer;
+  ::-webkit-details-marker {
+    display: none;
+  }
+  :focus,
+  :hover {
+    text-decoration: underline;
+  }
+  ::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    width: 6px;
+    height: 6px;
+    border-top: 1px solid #000;
+    border-left: 1px solid #000;
+    transform: translateY(-50%) rotate(135deg);
+    transition: transform 300ms ease;
+  }
+  ${Details}[open] &::before {
+    transform: translateY(-50%) rotate(225deg);
+  }
 `
